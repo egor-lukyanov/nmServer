ef12cdd1cfedf610fcce1a836625ec7ba35d4fc0
egor-lukyanov <elukianov@isgneuro.com>
2022-05-22 11:11:39 +0300

    host & port

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from app import app
 
+host = 'localhost'
+port = 5000
+
 if __name__ == '__main__':
     from input import bp as input_eps
     from output import bp as output_eps
     app.register_blueprint(input_eps)
     app.register_blueprint(output_eps)
-    app.run(debug=True)
+    app.run(host=host, port=port, debug=True)
